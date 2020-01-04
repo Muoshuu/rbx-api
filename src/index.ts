0d731841d90e0000c6799a4308f2baa57ac7c30d
Muoshuu <muoshuu@gmail.com>
2020-01-03 20:01:46 -0600

    Added compression

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,11 @@ import * as express from 'express';
 import { serveProxy } from './proxy';
 import { serveReflection } from './reflection/.';
 
+const compression = require('compression')();
+
 const app = express(); {
+	app.use(compression);
+	
 	serveProxy(app);
 	serveReflection(app);
 }
