4e2f718bb9efac56cba1882711ccc56a792f2ae2
Muoshuu <muoshuu@gmail.com>
2020-01-03 20:04:00 -0600

    Keep heroku from sleeping

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
 import * as express from 'express';
 import { serveProxy } from './proxy';
 import { serveReflection } from './reflection/.';
+import request = require('request-promise');
 
 const compression = require('compression')();
 
 const app = express(); {
 	app.use(compression);
-	
+
 	serveProxy(app);
 	serveReflection(app);
+}
+
+if (process.env.PORT) {
+	setInterval(() => {
+		request('https://rbx-api.herokuapp.com/dump/classes/Part/members');
+	}, 600000);
 }
 
 app.listen(process.env.PORT || 5000);
